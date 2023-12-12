0149e540dfd817aaa1efbd6fbc8ffc156ddb8ea1
Aleksey <pavlinov24121990@gmail.com>
2023-12-12 13:24:41 +0100

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,107 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+type Operation = "+" | "-" | "*" | "/";
+
+const Home: React.FC = () => {
+  const [answer, setAnswer] = useState<number | Operation>(0)
+  const [number1, setNumber1] = useState<number | null>(null)
+  const [number2, setNumber2] = useState<number | null>(null)
+  const [operator, setOperator] = useState<Operation | null>(null);
+
+  function calculate(operator: Operation, number1: number, number2: number): number {
+    switch (operator) {
+      case "+":
+        return number1 + number2;
+      case "-":
+        return number1 - number2;
+      case "*":
+        return number1 * number2;
+      case "/":
+        if (number2 !== 0) {
+            return number1 / number2;
+        } else {
+            throw new Error("Деление на ноль невозможно");
+        }
+    }
+  }
+  
+  const saveNumberClick = (number: number): void => {
+    if (operator === null) {
+      setNumber1((prevNumber) => (prevNumber !== null ? prevNumber * 10 + number : number));
+    } else {
+      setNumber2((prevNumber) => (prevNumber !== null ? prevNumber * 10 + number : number));
+    }
+  }
+
+  useEffect(() => {
+    if (number1 !== null) {
+      setAnswer(number1)
+    }
+    if (operator !== null) {
+      setAnswer(operator)
+    }
+    if (number2 !== null) {
+      setAnswer(number2)
+    }
+  }, [number1, number2, operator]);
+
+  const saveOperatorClick = (operator: Operation): void => {
+    setOperator(operator)
+  }
+
+  const clearAllState = (): void => {
+    setAnswer(0)
+    setNumber1(null)
+    setNumber2(null)
+    setOperator(null)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="d-flex flex-column align-items-center">
+      <div className="p-5 mb-5 main">{answer}</div>
+      <div className='d-flex'>
+        <div className="me-5">
+          <ul className="list-unstyled column-list">
+            {[7, 8, 9, 4, 5, 6, 1, 2, 3].map((number) => (
+              <li key={number}>
+                <button onClick={() => saveNumberClick(number)}>{number}</button>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <ul className="list-unstyled operator-list">
+             {["+", "-", "*", "/"].map((operator) => (
+              <li key={operator}>
+                <button onClick={() => saveOperatorClick(operator as Operation)}>{operator}</button>
+              </li>
+            ))}
+            <li>
+              <button onClick={() => {
+                try {
+                  if (operator && number1 !== null && number2 !== null) {
+                    const result = calculate(operator, number1, number2);
+                    setAnswer(result)
+                    setNumber1(null)
+                    setNumber2(null)
+                    setOperator(null)
+                  } 
+                } catch (error) {
+                  console.error('Ошибка');
+                }
+                }}>
+                =
+              </button>
+            </li>
+            <li>
+              <button onClick={() => clearAllState()}>C</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   )
 }
+
+export default Home;
